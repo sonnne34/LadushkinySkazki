32ddf08eab7270bda677e7fb64a5e1cf419538c7
sonne <sonnne34@gmil.com>
2022-07-06 15:57:39 +0500

    edit

--- app/src/main/java/com/ladushkinySkazky/ladushkinnyskazki/snake/SnakeActivity.kt
+++ app/src/main/java/com/ladushkinySkazky/ladushkinnyskazki/snake/SnakeActivity.kt
@@ -132,8 +132,8 @@ class SnakeActivity : AppCompatActivity() {
     //координаты нового объекта "еды"
     private fun generateAnimalCoordinates(): ViewCoordinate {
         val viewCoordinate = ViewCoordinate(
-            (0 until CELLS_ON_GENERATE).random() * loadTextHead().toInt(),
-            (0 until CELLS_ON_GENERATE).random() * loadTextHead().toInt()
+            (1 until CELLS_ON_GENERATE).random() * loadTextHead().toInt(),
+            (1 until CELLS_ON_GENERATE).random() * loadTextHead().toInt()
         )
         for (partTale in allTale) {
             if (partTale.viewCoordinate == viewCoordinate) {
@@ -201,8 +201,8 @@ class SnakeActivity : AppCompatActivity() {
     //координаты нового объекта кроватка
     private fun generateBedCoordinates(): ViewCoordinate {
         val viewCoordinate = ViewCoordinate(
-            (0 until CELLS_ON_GENERATE).random() * loadTextHead().toInt(),
-            (0 until CELLS_ON_GENERATE).random() * loadTextHead().toInt()
+            (1 until CELLS_ON_GENERATE).random() * loadTextHead().toInt(),
+            (1 until CELLS_ON_GENERATE).random() * loadTextHead().toInt()
         )
         for (partTale in allTale) {
             if (partTale.viewCoordinate == viewCoordinate) {
